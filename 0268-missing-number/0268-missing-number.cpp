5cba54e4d6bccdd6befcd354aa1c643bb4e9a5d9
Abhishek Kumar Sharma <learner.m1729@gmail.com>
2026-08-11 23:00:09 +0530

    Time: 0 ms (100%), Space: 21.9 MB (39.34%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int acc_sum = (n * (n + 1)) / 2;
+
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        return acc_sum - sum;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
